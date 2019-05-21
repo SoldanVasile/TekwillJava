d5b3d6de3ab7350288523de598bf53a1cdb00acc
Soldan Vasile <soldan.vasile@gmail.com>
2019-05-21 11:01:41 +0300

    am creat mapa Lesson 4 si in interiorul acesteia, fisierul de tip java cu declararea si initierea variabelelor

---
+++ Lesson 4/LearningVariables.java
@@ -0,0 +1,16 @@
+public class LearningVariables {
+
+   public static void main(String [] args) {
+  
+      	boolean userAnswer = true;
+      	double piConstant = 3.14;
+	String wellcomeMesage = "Wellcome!";
+
+
+	int perimetreOfFigure, arriaOfFigure, lenghtOfEdge;
+	string errorMesage, commentMesage, netName;
+	boolean isPaswordActivate, isInternetConection, isUserLoginn;
+
+  }
+
+}
