55d4bbe6c85ac950ded3e39b67ff69759f3b1562
Soldan Vasile <soldan.vasile@gmail.com>
2019-06-06 12:48:57 +0300

    Am adaugat codul pentru aplicatia ce identifica tipul literei introduse

---
+++ Lesson 10 Packages and Imports/HomeWork Lesson 10/IdentificaLitera.java
@@ -0,0 +1,99 @@
+//importam classa scaner din utility java
+
+import java.util.Scanner;
+
+
+public class IdentificaLitera {
+
+	public static void main(String [] args) {
+
+		
+	//introducere unui mesaj de salut, cu solicitarea de a introduce o litera alfabetului latin
+		
+		System.out.println("Salut, introdu o litera a alfabetului latin si apasa tasta Enter");
+	
+	// creare obiect nou din clasa scanner pentru a putea introduce informatii de la tastatura calculatorului
+
+		Scanner scanner = new Scanner(System.in);
+
+
+		String simbol = scanner.nextLine();
+
+
+		//stabilim ca lungimea sirului sa fie egala cu 1, ca sa fie posibila introducerea unei singure litere
+
+		if (simbol.length() > 1) {
+			System.out.println("Ati introdus mai mult de o litera sau simboluri alfanumerice, reveniti si introduceti o sinura litera");
+		
+		} else {
+			//vocalele alfabetului latin: A, E, I, O, U
+
+			switch(simbol) {
+	
+				case "A" :
+				case "E" :
+				case "I" :
+				case "O" :
+				case "U" :
+				case "a" :
+				case "e" :
+				case "i" :
+				case "o" :
+				case "u" :
+
+				System.out.println("Litera " + simbol + " este o vocala");
+				break;
+
+				case "B" :
+				case "C" :
+				case "D" :
+				case "F" :
+				case "G" :
+				case "H" :
+				case "J" :
+				case "K" :
+				case "L" :
+				case "M" :
+				case "N" :
+				case "P" :
+				case "Q" :
+				case "R" :
+				case "S" :
+				case "T" :
+				case "V" :
+				case "W" :
+				case "X" :
+				case "Y" :
+				case "Z" :
+				case "b" :
+				case "c" :
+				case "d" :
+				case "f" :
+				case "g" :
+				case "h" :
+				case "j" :
+				case "k" :
+				case "l" :
+				case "m" :
+				case "n" :
+				case "p" :
+				case "q" :
+				case "r" :
+				case "s" :
+				case "t" :
+				case "v" :
+				case "w" :
+				case "x" :
+				case "y" :
+				case "z" :
+				
+				System.out.println("Litera " + simbol + " este o consoana");
+				break;
+
+			default:
+				System.out.println("Simbolul " + simbol + " nu este o litera a alfabetului latin");
+
+			}
+		} 
+	}
+}
