989beafbe641a5bac0be6d3e788ff4a4c504f2f6
Soldan Vasile <soldan.vasile@gmail.com>
2019-06-06 21:32:57 +0300

    Am adaugat fisierul cu cod ce realizeaza suma la doua numere de tip int, double si float

---
+++ Lesson 10 Packages and Imports/HomeWork Lesson 10/Suma.java
@@ -0,0 +1,75 @@
+//importam classa scaner din utility java
+
+import java.util.Scanner;
+
+
+public class Suma {
+
+	public static void main(String [] args) {
+
+	
+	//introducere unui mesaj de salut, cu solicitarea de a introduce primul numar 
+		
+		System.out.println("Salut, introdu primul numar intreg si apasa tasta ENTER");
+	
+	// creare obiect nou din clasa scanner pentru a putea introduce informatii de la tastatura calculatorului
+
+		Scanner scanner = new Scanner(System.in);
+
+
+		int numix = scanner.nextInt();
+	
+	//introducere unui mesaj cu solicitarea de a introduce al doilea numar 	
+		System.out.println("Acum introdu al doilea numar intreg si apasa tasta ENTER");
+		int numiy = scanner.nextInt();
+
+		int sumaixy = numix + numiy;
+
+	//afiseaza suma celor doua numere
+
+		System.out.println("Suma numerelor intregi " + numix + " si " + numiy + " este " + sumaixy);
+
+
+//partea a doua, cu numere de tip double
+
+//introducere unui mesaj de salut, cu solicitarea de a introduce primul numar 
+		
+		System.out.println("Salut, introdu primul numar de tip double si apasa tasta ENTER");
+	
+	// creare obiect nou din clasa scanner pentru a putea introduce informatii de la tastatura calculatorului
+
+		double numdx = scanner.nextDouble();
+	
+	//introducere unui mesaj cu solicitarea de a introduce al doilea numar 	
+		System.out.println("Acum introdu al doilea numar de tip double si apasa tasta ENTER");
+		double numdy = scanner.nextDouble();
+
+		double sumadxy = numdx + numdy;
+
+	//afiseaza suma celor doua numere
+
+		System.out.println("Suma numerelor de tip double " + numdx + " si " + numdy + " este " + sumadxy);
+
+
+//partea a treia, cu numere de tip floate
+
+//introducere unui mesaj de salut, cu solicitarea de a introduce primul numar 
+		
+		System.out.println("Salut, introdu primul numar de tip float si apasa tasta ENTER");
+	
+	// creare obiect nou din clasa scanner pentru a putea introduce informatii de la tastatura calculatorului
+
+		float numfx = scanner.nextFloat();
+	
+	//introducere unui mesaj cu solicitarea de a introduce al doilea numar 	
+		System.out.println("Acum introdu al doilea numar de tip float si apasa tasta ENTER");
+		float numfy = scanner.nextFloat();
+
+		float sumafxy = numfx + numfy;
+
+	//afiseaza suma celor doua numere
+
+		System.out.println("Suma numerelor de tip float " + numfx + " si " + numfy + " este " + sumafxy);
+
+	}
+}
